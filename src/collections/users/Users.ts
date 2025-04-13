787ebefc3555e372729056ecf5b88666ceeb3d57
Norbert Balaz <n.balaz@outlook.com>
2025-04-14 01:38:23 +0200

    feat: extends user with name field

--- src/collections/users/Users.ts
+++ src/collections/users/Users.ts
@@ -3,13 +3,17 @@ import type { CollectionConfig } from 'payload'
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: 'name',
     group: 'Access',
+    defaultColumns: ['name', 'email'],
     description: 'Control user access, roles, and credentials for managing the platform.',
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'name',
+      type: 'text',
+    },
   ],
+  timestamps: true,
 }
